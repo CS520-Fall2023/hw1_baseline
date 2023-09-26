6ac809857670a147e3eeee911772eb35d75bb488
Akhila Reddy <akhilareddy@vl965-172-31-167-8.wireless.umass.edu>
2023-09-26 16:02:35 -0400

    inputValidation.java added

---
+++ expense_tracker/src/InputValidation.java
@@ -0,0 +1,21 @@
+public class InputValidation{
+    
+    public static boolean isValidAmount(double amount){
+        try {
+            return amount > 0 && amount < 1000;
+        } 
+        catch (NumberFormatException e) {
+            return false;
+        }
+    }
+    public static boolean isValidCategory(String category) {
+        String[] validCategories = {"food", "travel", "bills", "entertainment", "other"};
+        for (String input : validCategories) {
+            if (category.equalsIgnoreCase(input)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
